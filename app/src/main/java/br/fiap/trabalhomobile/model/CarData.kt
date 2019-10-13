a8126c7cc2648438edde9a5f1e452fd4a89ffde9
ThiagoLangoni <loucoporbike@hotmail.com>
2019-10-13 00:42:05 -0300

    Fontes do Trabalho :)

--- app/src/main/java/br/fiap/trabalhomobile/model/CarData.kt
+++
@@ -1,8 +0,0 @@
-package br.fiap.trabalhomobile.model
-
-data class CarData(
-    val gasPrice: Double = 0.0,
-    val ethanolPrice: Double = 0.0,
-    val gasAverage: Double = 0.0,
-    val ethanolAverage: Double = 0.0
-)
